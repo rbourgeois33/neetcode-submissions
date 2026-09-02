83fbb45fdee68f10d01d34a8091a4b1d5b652ad0
moletteremi <98650511+rbourgeois33@users.noreply.github.com>
2026-09-02 14:26:01 +0200

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.cpp
@@ -0,0 +1,41 @@
+bool isClosing(const char c){
+    return (c=='}'||c==')'||c==']');
+}
+
+bool isClosingAndMatches(const char c, const std::stack<char>& stack){
+     if ((c==')') && (stack.top()=='(')) return true;
+     if ((c==']') && (stack.top()=='[')) return true;
+     if ((c=='}') && (stack.top()=='{')) return true;
+
+     return false;
+}
+
+class Solution {
+public:
+    bool isValid(string s) {
+
+        std::stack<char> stack;
+
+        //if (s.length()%2!=0) return false;
+        
+        for (auto c:s){
+            
+            if (isClosing(c)){
+                
+                if (stack.empty()) return false;
+
+                if (isClosingAndMatches(c, stack)){
+                    stack.pop();
+                }else{
+                    return false;
+                }
+               
+            }else{
+                stack.push(c);
+            }
+        }
+    
+
+        return stack.empty();
+    }
+};
